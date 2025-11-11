a2d1415bb6b90c3780c3cb67dbff80b0db253e51
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2025-11-11 15:19:44 +0100

    fixed navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,16 +12,16 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="/about">About</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/services">Services</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/contact">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="btn btn-primary" href="/contact">Get Started</a>
+                        <a class="btn btn-primary" href="/contact">Contact</a>
                     </li>
                 </ul>
             </div>
